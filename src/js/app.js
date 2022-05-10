288697301291618e08a60e1d6fae054675c7b05f
nioritos <junioradr2008@gmail.com>
2022-05-10 12:43:02 -0300

    adjajksdnasdh asd asda sdbasd asdb asd asd

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,51 @@ function onScroll() {
     const nav = document.querySelector("nav");
     const strongTitle = document.querySelector(".logo-title strong");
     const btToggle = document.querySelector(".bt-toggle");
+    const home = document.querySelector("#home");
+    const services = document.querySelector("#services").clientHeight;
         if (scrollY > header.clientHeight) {
         header.classList.add('turn')
         nav.classList.add('turn')
         strongTitle.classList.add('colored');
         btToggle.innerHTML = '<img src="./assets/svg/Icon-light.svg" alt="icone para mostrar o menu mobile" class="icon-menu">'
-    } else {
+    }else if (scrollY < home.clientHeight){
+        header.classList.remove('turn')
+        nav.classList.remove('turn')
+        strongTitle.classList.remove('colored');
+        btToggle.innerHTML = '<img src="./assets/svg/Icon-dark.svg" alt="icone para mostrar o menu mobile" class="icon-menu">'
+    }else if (scrollY > services) {
+        const btScroll = document.querySelector(".scroll-to-home");
+        btScroll.classList.remove('none-bt');
+        btScroll.classList.add('showed-bt');
+        btToggle.addEventListener('click', () => {
+            
+        })
+    }
+    
+    else {
         return false
     }
+
+    // for(let i = header.clientHeight; scrollY > i; i++) {
+    //     header.classList.add('turn')
+    //     nav.classList.add('turn')
+    //     strongTitle.classList.add('colored');
+    //     btToggle.innerHTML = '<img src="./assets/svg/Icon-light.svg" alt="icone para mostrar o menu mobile" class="icon-menu">'
+    // }
 };
+
+function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    };
+
+
 const menu = [...document.querySelectorAll('#get-element')]
-const elementsMenu = [...document.querySelectorAll('#get-element li, #get-element a')];
+const elementsMenu = [...document.querySelectorAll('#get-element li')];
+const btNone = [...document.querySelectorAll("#get-button ")];
 
 
 Modal = {
@@ -28,16 +62,23 @@ Modal = {
         elementsMenu.forEach(item => {
             item.classList.add('showed')
         })
+
+        btNone.forEach(item => {
+            item.classList.add('showed')
+        })
     },
 
     toggleNone: () => {
         menu.forEach(item => {
             item.classList.remove('toggled')
 
-            elementsMenu.forEach(item => {
-                item.classList.remove('showed')
-            })
         })
+        elementsMenu.forEach(item => {
+            item.classList.remove('showed')
+        })
+
+        btNone.classList.remove('showed');
+
 
     }
 
