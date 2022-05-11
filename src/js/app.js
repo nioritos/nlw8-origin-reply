e9735532f03e7750d25baa54b5461b77fb40de41
nioritos <junioradr2008@gmail.com>
2022-05-11 09:52:15 -0300

    more updates to files and the project.

--- src/js/app.js
+++ src/js/app.js
@@ -6,29 +6,33 @@ function onScroll() {
     const btToggle = document.querySelector(".bt-toggle");
     const home = document.querySelector("#home");
     const services = document.querySelector("#services").clientHeight;
-        if (scrollY > header.clientHeight) {
+    const btScroll = document.querySelector(".scroll-to-home");
+        
+    
+    if (scrollY > header.clientHeight) {
         header.classList.add('turn')
         nav.classList.add('turn')
         strongTitle.classList.add('colored');
         btToggle.innerHTML = '<img src="./assets/svg/Icon-light.svg" alt="icone para mostrar o menu mobile" class="icon-menu">'
-    }else if (scrollY < home.clientHeight){
+     }//else if (scrollY > home.clientHeight) {
+    //     btScroll.classList.remove('none-bt');
+    //     btScroll.classList.add('showed-bt');
+    //     btToggle.addEventListener('click', scrollToTop());
+    // }
+    else if (scrollY < home.clientHeight){
         header.classList.remove('turn')
         nav.classList.remove('turn')
         strongTitle.classList.remove('colored');
         btToggle.innerHTML = '<img src="./assets/svg/Icon-dark.svg" alt="icone para mostrar o menu mobile" class="icon-menu">'
-    }else if (scrollY > services) {
-        const btScroll = document.querySelector(".scroll-to-home");
-        btScroll.classList.remove('none-bt');
-        btScroll.classList.add('showed-bt');
-        btToggle.addEventListener('click', () => {
-            
-        })
-    }
-    
-    else {
+        btScroll.classList.remove('showed-bt')
+    }else {
         return false
     }
 
+    while(scrollY > services) {
+        btScroll.classList.remove('none-bt');
+        btScroll.classList.add('showed-bt');
+    }
     // for(let i = header.clientHeight; scrollY > i; i++) {
     //     header.classList.add('turn')
     //     nav.classList.add('turn')
